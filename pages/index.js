794ec2e57c4deae45e7b6a303ea0ec222c6c9704
krish5555 <ka36l1107@gmail.com>
2022-07-09 04:08:07 +0530

    Product list

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import Layout from '../components/layout'
+import ProductItems from '../components/productItems'
+import data from '../utils/data'
 
 export default function Home() {
   return (
-    <Layout title={"Home page"}>Home page</Layout>
+    // <Layout title={"Home page"}>Home page</Layout>
+    <Layout title={"Home page"}>
+      <div className='grid drid-col-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
+      {data.products.map((product) => (
+        <ProductItems product={product} key={product.slug}></ProductItems>
+      ))}
+      </div>
+    </Layout>
   )
 }
